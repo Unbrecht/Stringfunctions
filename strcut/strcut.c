5aaf6c50abe2ec80870dd2e921f7ab041bfdca2d
Erik <84043548+Unbrecht@users.noreply.github.com>
2023-03-22 08:08:40 +0100

    Update strcut.c

--- strcut/strcut.c
+++ strcut/strcut.c
@@ -1,3 +1,7 @@
+//I use this function on my uC. They get flash data from an ESP8266 over Browser. When content data >1460 a new Packet will transmit. 
+//The ESP8266 prints the Socket Info "IPD,SOCKET,PACKETSIZE:DATA. To remove this string i created function above. 
+//The strcut function will not clear the complete String. After clearing and moving substring a '\0' will add to end string.
+	
 #include <inttypes.h>
 
 //Optional, you can use strlen. But for my reasons i create my own
